9b72eb6a750b40066d3b05e976fe8fd564566467
Artem Kutsan <artemkutsan@gmail.com>
2026-07-02 16:32:14 +0200

    feat: подключил meal types query reducer, cuisines query reducer и recipe query reducer к store

--- src/app/providers/store/index.js
+++ src/app/providers/store/index.js
@@ -1,4 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { mealTypesApi } from '@/entities/meal-type';
+import { cuisinesApi } from '@/entities/cuisine';
+import { recipeApi } from '@/entities/recipe/api/recipesQuery';
 import { recipesReducer } from '@/entities/recipe';
 import { usersReducer } from '@/entities/user';
 import { addRecipeReducer } from '@/features/add-recipe';
@@ -10,5 +13,10 @@ export const store = configureStore({
     users: usersReducer,
     addRecipe: addRecipeReducer,
     mealPlan: mealPlanReducer,
+    [mealTypesApi.reducerPath]: mealTypesApi.reducer,
+    [cuisinesApi.reducerPath]: cuisinesApi.reducer,
+    [recipeApi.reducerPath]: recipeApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(mealTypesApi.middleware, cuisinesApi.middleware, recipeApi.middleware),
 });
