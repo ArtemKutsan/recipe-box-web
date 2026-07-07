a89cc0210c05b4d1db4ec4a190b88ec69d0bacd5
Artem Kutsan <artemkutsan@gmail.com>
2026-07-07 15:53:36 +0200

    chore: недначительное обновление стилей

--- src/shared/ui/Badge/index.jsx
+++ src/shared/ui/Badge/index.jsx
@@ -4,7 +4,7 @@ const Badge = ({ children, className = '' }) => {
   return (
     <span
       className={cn(
-        'w-fit rounded-full bg-secondary/5 px-4 py-2 text-sm text-secondary',
+        'w-fit lowercase rounded-full bg-secondary/5 px-2 py-1 text-sm text-secondary',
         className,
       )}
     >
