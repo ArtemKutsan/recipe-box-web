0e4b1ac9cc52be8f9337df69b36010596e20c0d8
Artem Kutsan <artemkutsan@gmail.com>
2026-09-14 13:08:38 +0200

    chore: уточнил комментарии

--- src/features/recipe-filters/index.js
+++ src/features/recipe-filters/index.js
@@ -1,4 +1,4 @@
-// Компоненты и функции для фильтрации рецептов по категориям (кухня и тип блюда)
+// Компоненты и функции для фильтрации рецептов по типу блюда и кухне.
 export { default as CuisineCarousel } from './ui/CuisineCarousel';
 export { default as MealTypeCarousel } from './ui/MealTypeCarousel';
 export { default as CuisineSelect } from './ui/CuisineSelect';
