99ce7f1e37f9b2baeeafe5514c5cff84fdc1e185
Artem Kutsan <artemkutsan@gmail.com>
2026-09-14 13:02:06 +0200

    refactor: уточнил публичный API recipe-discovery

--- src/features/recipe-discovery/index.js
+++ src/features/recipe-discovery/index.js
@@ -1,4 +1,2 @@
 export { default as RecipeFilters } from './ui/RecipeFilters';
 export { default as RecipeListControls } from './ui/RecipeListControls';
-export { default as RecipeSearch } from './ui/RecipeSearch';
-export { default as ClearFiltersButton } from './ui/ClearFiltersButton';
